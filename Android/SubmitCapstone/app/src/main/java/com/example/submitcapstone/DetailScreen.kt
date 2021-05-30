ea47132a83c2fc540d20038aae98461dc0c24d12
Alfred Tambayong <alfred.tambayong@binus.ac.id>
2021-05-30 17:39:58 +0700

    test

--- Android/SubmitCapstone/app/src/main/java/com/example/submitcapstone/DetailScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.submitcapstone
-
-class DetailScreen : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_screen)
-    }
-}
